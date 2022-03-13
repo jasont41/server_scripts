1cced1e12035b70dd7ac785a8034d047a06a1daa
jasont41 <jasont41@gmail.com>
2022-03-13 09:17:05 +0000

    working sans passwords and shit

--- vpn-bump.py
+++ vpn-bump.py
@@ -1,1 +1,21 @@
 import paramiko as p
+#from subprocess import stdout
+
+def send_command(host, username, password): 
+   ssh = p.SSHClient() 
+   ssh.set_missing_host_key_policy(p.AutoAddPolicy())
+   ssh.connect(ip,port,user,password,timeout=5)
+   stdin, stdout, stderr = ssh.exec_command("df")
+   outline = stdout.readlines()
+   resp = ''.join(outline)
+   print(resp)
+   ssh.close()
+
+
+def main():
+    #   This is main 
+    send_command("192.168.254.11","jason","1needEmail")
+    print("Just a print for now")
+
+if __name__ == "__main__":
+    main()
