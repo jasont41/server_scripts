226e05e12cfa3257ae8248b802306433959da2ea
jasont41 <jasont41@gmail.com>
2022-03-19 22:21:48 +0000

    logging working, need to build out more

--- ssh_script.py
+++ ssh_script.py
@@ -4,17 +4,21 @@ from os import lseek
 import paramiko as p
 import yaml
 import sys 
+import logging 
 
 class ssh_instance: 
-    def __init__(self, incoming_state):
+    def __init__(self, incoming_state,argv):
+        logging.basicConfig(filename='../logging/ssh_script.log', format='%(asctime)s %(message)s',level=logging.DEBUG)
+        #   To cut down on BS paramiko logs 
+        logging.getLogger("paramiko").setLevel(logging.WARNING)
         ssh_instance.state = incoming_state 
         ssh_instance.print_state(self)
         if len(sys.argv) > 2:
-            print("Too many arguments")
+            logging.debug("Too many arguments")
             sys.exit()
+        logging.debug("Opening Config File")
         config_file = open("../ssh_config.yaml",'r')
         #   open file for logging 
-        log_file = self.start_log()
         #   grab config and return into a dictionary 
         yaml_dict = self.get_config(config_file)  
         self.send_command(yaml_dict['host'], yaml_dict['user'], yaml_dict['password'])
@@ -32,7 +36,7 @@ class ssh_instance:
         stdin, stdout, stderr = ssh.exec_command("df")
         outline = stdout.readlines()
         resp = ''.join(outline)
-        print(resp)
+        logging.debug(resp)
         ssh.close()
     '''
     #   Description: Retrieves credentials from yaml file  
@@ -40,7 +44,7 @@ class ssh_instance:
     '''
     def get_config(self, config):
         print("Getting SSH credential...")
-        _config = yaml.load(config)
+        _config = yaml.safe_load(config)
         return _config
     '''
     #    Description: Opens file for logging, creates file if file doesn't exist 
@@ -61,7 +65,7 @@ class ssh_instance:
 
 
 def main(argv):
-    test = ssh_instance ("test")
+    test = ssh_instance ("test",argv)
 
 
 if __name__ == "__main__":
