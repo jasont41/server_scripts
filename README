458a3fa914b6f1729a991c649f8f0a1a26851f4c
jasont41 <jasont41@gmail.com>
2022-03-20 08:29:25 +0000

    needs work

---
+++ README
@@ -0,0 +1,12 @@
+This is a command line SSH application. I am building more to get more accustomed to python rather than build an app that I think someone will actually use. I plan to build full functionality as if someone wanted to actually use this tool, and eventually I might build a GUI for it. Who knows. 
+
+
+<h3>Flags</h3>
+<ul>
+<li>-c
+<ul>
+<li>Using this flag with allow the user to retype their credentials into the config yaml</li>
+</ul>
+</li>
+
+</ul>
--- ssh_script.py
+++ ssh_script.py
@@ -1,5 +1,6 @@
 from asyncio.log import logger
 from asyncore import file_dispatcher
+from distutils.command.config import config
 from genericpath import exists
 from os import lseek
 import paramiko as p
@@ -8,18 +9,23 @@ import sys
 import logging 
 
 class ssh_instance: 
+    config_file = {}
+    yaml_loc = "../ssh_config.yaml"
     def __init__(self, argv):
         logging.basicConfig(filename='../logging/ssh_script.log', format='%(asctime)s %(message)s',level=logging.DEBUG)
         #   To cut down on BS paramiko logs 
         logging.getLogger("paramiko").setLevel(logging.WARNING)
-        if len(sys.argv) > 2:
-            logging.debug("Too many arguments")
-            sys.exit()
+        if(len(sys.argv) > 1):
+            self.check_flags(argv)
         logging.debug("Opening Config File")
-        config_file = open("../ssh_config.yaml",'r')
+        #ssh_instance.yaml_loc = "../ssh_config.yaml"
+        ssh_instance.config_file = open(self.yaml_loc,'r')
+        print(ssh_instance.config_file)
         #   grab config and return into a dictionary 
-        yaml_dict = self.get_config(config_file)  
-        self.send_command(yaml_dict['host'], yaml_dict['user'], yaml_dict['password'])
+        self.yaml_dict = self.get_config(self.config_file)  
+        if(len(sys.argv) > 1):
+            self.check_flags(argv)
+        #self.send_command(yaml_dict['host'], yaml_dict['user'], yaml_dict['password'])
 
     
     '''
@@ -50,10 +56,47 @@ class ssh_instance:
     def get_config(self, config):
         logger.info("Getting SSH credentials...")
         _config = yaml.safe_load(config)
+        print(_config)
         return _config
+    '''
+    #   Description: Checks incoming flags and calls respective function 
+    #   Returns:     Nothing 
+    '''
+    def check_flags(self, argv):
+        if sys.argv[1] == "-c":
+            self.change_credentials()
+        else: 
+            logging.warn("Invalid flag, Aborting program")
+    '''
+    #   Description: Changes SSH credentials   
+    #   Returns:     Nothing 
+    '''
+    def change_credentials(self):
+        logger.info("Time to change SSH Credentials")
+        username = ""
+        _password = ""
+        while len(username) == 0: 
+            username = input("Enter Username\n")
+            if len(username) == 0: 
+                print("Enter a username!\n")
+        print("\n\n")
+        while len(_password) == 0: 
+            _password = input("Enter Password\n")
+            if len(_password) == 0: 
+                print("Enter a password!\n")
+        self.save_yaml(username,_password)
+        #logging.info("Username and Password changed")
+        
+    def save_yaml(self,username,password): 
+        self.config_file["user"] = username
+        self.config_file["password"] = password
+        print(self.config_file)
+        with open(self.yaml_loc, 'w') as f: 
+            yaml.dump(self.config_file,f)
         
 def main(argv):
     test = ssh_instance (argv)
+
 
 
 if __name__ == "__main__":
